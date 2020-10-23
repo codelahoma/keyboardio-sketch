49b6968f099be71a6c3ad10f70544270be51c96e
Rod Knowlton <rod.knowlton@summitesp.com>
2020-10-23 08:12:09 -0500

    added special modifiers

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -176,12 +176,12 @@ KEYMAPS(
    Key_Escape, Key_Backspace, OSM(LeftGui), OSM(LeftShift),
    OSL(FUNCTION),
 
-   Key_AmethystMainWindow,  Key_6, Key_7, Key_8, Key_9, Key_0, LGUI(Key_Backtick),
+   LGUI(Key_H),  Key_6, Key_7, Key_8, Key_9, Key_0, LGUI(Key_Backtick),
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    OSL(FUNCTION),  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
    OSM(LeftShift), OSM(LeftAlt), Key_Spacebar, OSM(LeftControl),
-   OSM(LeftGui)),
+   OSL(FUNCTION)),
 
 #else
 
@@ -341,11 +341,21 @@ KALEIDOSCOPE_INIT_PLUGINS(
 void setup() {
   Kaleidoscope.setup();
   QUKEYS(
+/* #define PER_KEY_DATA(dflt,
+//   r0c0, r0c1, r0c2, r0c3, r0c4, r0c5, r0c6,        r0c9,  r0c10, r0c11, r0c12, r0c13, r0c14, r0c15,
+//   r1c0, r1c1, r1c2, r1c3, r1c4, r1c5, r1c6,        r1c9,  r1c10, r1c11, r1c12, r1c13, r1c14, r1c15,
+//   r2c0, r2c1, r2c2, r2c3, r2c4, r2c5,                     r2c10, r2c11, r2c12, r2c13, r2c14, r2c15,
+//   r3c0, r3c1, r3c2, r3c3, r3c4, r3c5, r2c6,        r2c9,  r3c10, r3c11, r3c12, r3c13, r3c14, r3c15,
+//               r0c7, r1c7, r2c7, r3c7,                             r3c8,  r2c8,  r1c8, r0c8,
+//                           r3c6,                                          r3c9, ...)                      */
+
          kaleidoscope::plugin::Qukey(0, KeyAddr(0, 7), Key_LeftControl),
          kaleidoscope::plugin::Qukey(0, KeyAddr(1, 0), Key_CommandShift),
          kaleidoscope::plugin::Qukey(0, KeyAddr(2, 0), Key_Magic),
+         kaleidoscope::plugin::Qukey(0, KeyAddr(2, 6), Key_CommandShift),
+         kaleidoscope::plugin::Qukey(0, KeyAddr(2, 9), Key_CommandShift),
          kaleidoscope::plugin::Qukey(0, KeyAddr(3, 0), Key_MyHyper),
-         kaleidoscope::plugin::Qukey(0, KeyAddr(3, 9), ShiftToLayer(FUNCTION))
+         kaleidoscope::plugin::Qukey(0, KeyAddr(3, 9), Key_LeftGui)
          );
   Qukeys.setHoldTimeout(200);
   Qukeys.setOverlapThreshold(25);
